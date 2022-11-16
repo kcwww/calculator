45fc5b9b48a501498b9b18683c61e621c929e5bd
HyeJiJUN <jhj910421@gmail.com>
2022-11-16 19:40:35 +0900

    complete

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
 CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
