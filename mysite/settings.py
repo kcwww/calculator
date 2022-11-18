9f208759bbd8b874c8e225449fb7b9348e9c859f
Chanwoo Kim <chanwookim@Chanwooui-MacBookPro.local>
2022-11-18 16:49:02 +0900

    django

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,8 +67,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = myDATABASES
-
+#DATABASES = myDATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER' : 'root',
+        'PASSWORD' : MY_DB_PASSWORD,
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        }
+}
 
 
 # Password validation
